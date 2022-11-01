6ef7f2e6a2e874c31998f43372ee4a67d8656c32
Matilda <afemalecoder@gmail.com>
2022-11-01 09:45:37 +0100

    Initial Commit

---
+++ GameFinder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GameFinder
+//
+//  Created by Matilda Cederberg on 01/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameFinder/GameFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameFinderApp.swift
+//  GameFinder
+//
+//  Created by Matilda Cederberg on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
