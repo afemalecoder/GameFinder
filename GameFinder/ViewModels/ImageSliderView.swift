2f4d7baaea4304e12574c103fd144d1dd199b799
Matilda <afemalecoder@gmail.com>
2022-12-21 10:43:30 +0100

    - Conflict in commit

---
+++ GameFinder/ViewModels/ImageSliderView.swift
@@ -0,0 +1,46 @@
+//
+//  ImageSliderView.swift
+//  GameFinder
+//
+//  Created by Matilda Cederberg on 12/12/2022.
+//
+
+import SwiftUI
+
+struct ImageSliderView: View {
+    let imageURL: TheGame
+    private let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
+    @State private var currentIndex = 0
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame( width: 60, height: 3)
+                .foregroundColor(.white)
+                .opacity(0.5)
+                .shadow(radius: 2)
+                .padding(.vertical, 20)
+               
+            TabView(selection: $currentIndex) {
+                ForEach(imageURL.screenshots ?? [TheGame.Screenshots]()) { screenshot in
+                    AsyncImage(url: URL(string: "https://images.igdb.com/igdb/image/upload/t_screenshot_big/\(screenshot.image_id).jpg")) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 450, height: 400)
+                            .cornerRadius(12)
+                    } placeholder: {
+                        Image("gameFinder")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                            .cornerRadius(12)
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        }
+        .background(Colors().backgroundColor)
+
+    }
+}
---
+++ GameFinder/ViewModels/ModesView.swift
@@ -0,0 +1,32 @@
+//
+//  GameModesView.swift
+//  GameFinder
+//
+//  Created by Matilda Cederberg on 01/12/2022.
+//
+
+import SwiftUI
+
+struct ModesView: View {
+    @State var currentgame : TheGame
+    
+    var body: some View {   
+        VStack(alignment: .leading, spacing: 2) {
+            Text("Modes: ")
+                .font(.system(size: 15))
+                .foregroundColor(.gray)
+            ScrollView(.horizontal) {
+                if currentgame.game_modes != nil {
+                    HStack {
+                        ForEach(currentgame.game_modes ?? [TheGame.Modes]()) { game in
+                            
+                            Text(game.name)
+                        }
+                    }
+                } else {
+                    Text("N/A")
+                }
+            }
+        }
+    }
+}
